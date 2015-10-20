2e678878ed5e0889110fe8472cc44e11b04656f8
racuelcer <cervr413@newschool.edu>
2015-10-20 17:20:35 -0400

    arduino
    code

---
+++ SENSOR13.ino
@@ -0,0 +1,39 @@
+
+ 
+#define Pecho 6
+#define Ptrig 7
+long duracion, distancia;   
+ 
+void setup() {                
+  Serial.begin (9600);       
+  pinMode(Pecho, INPUT);     
+  pinMode(Ptrig, OUTPUT);    
+  pinMode(13, 1);           
+  }
+  
+void loop() {
+  
+  digitalWrite(Ptrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Ptrig, HIGH);  
+  delayMicroseconds(10);
+  digitalWrite(Ptrig, LOW);
+  
+  duracion = pulseIn(Pecho, HIGH);
+  distancia = (duracion/2) / 29;            
+  
+  if (distancia >= 500 || distancia <= 0){  
+    Serial.println("---");                  
+  }
+  else {
+    Serial.print(distancia);           
+    Serial.println("cm");              
+    digitalWrite(13, 0);               
+  }
+  
+   if (distancia <= 10 && distancia >= 1){
+    digitalWrite(13, 1);                     
+    Serial.println("Alarma.......");         
+  }
+  delay(400);                               
+}
